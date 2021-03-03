254f0aba8a10f786949b61754fe78a3c2a304b05
teja1729 <58336479+teja1729@users.noreply.github.com>
2021-03-03 15:25:48 +0530

    Add files via upload

--- Arrays/KthMinMaxInArray.py
+++ Arrays/KthMinMaxInArray.py
@@ -4,7 +4,8 @@
 #for kth max arr[len(arr)-k] suppose len(arr) =10 and 1st max is last element which is 10-1=9
 #time complexity O(nlogn)
 def kthMinMaxInArray(array,K):
-    array=list(set(array))
+    array_set=set(array)
+    array=list(array_set)
     array=sorted(array)
     return array[k-1],array[len(array)-k]
 if __name__== '__main__':
@@ -13,6 +14,6 @@ if __name__== '__main__':
     kthmin,kthmax = kthMinMaxInArray(arr,k)
     print(f'{k}th max is {kthmax}')
     print(f'{k}th min is {kthmin}')
-#output:
+#output :
 #2th max is 231
 #2th min is 2
